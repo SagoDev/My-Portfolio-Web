22d6d36e25b974de5aeabedf90f9f02f37a5d73a
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-12-21 14:28:51 -0300

    Create profile section

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger_icon");
     menu.classList.toggle("open");
-    icon.classList.toggle("open");
 }
